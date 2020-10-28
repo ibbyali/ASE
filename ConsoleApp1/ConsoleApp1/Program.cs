470941d7b3e1909953373fca26e0fabbbb2c38d3
Ibrahim Ali <ibbyali117@hotmail.co.uk>
2020-10-28 18:31:22 +0000

    Practice work

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting....");
+            Program app = new Program();
+            app.go();
+            MessageBox.Show("Test", "Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            MessageBox.Show("Test1", "Title1", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        public void go()
+        {
+            Console.WriteLine("in go");
+        }
+    }
+}
